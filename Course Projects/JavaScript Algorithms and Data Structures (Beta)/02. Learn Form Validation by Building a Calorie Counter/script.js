173ed91b70287c4eae0a9cb7ce3694daa6ce0cd5
EBoyKun <gustavopita@protonmail.com>
2024-03-03 16:58:53 +0100

    last save step 49

--- Course Projects/JavaScript Algorithms and Data Structures (Beta)/02. Learn Form Validation by Building a Calorie Counter/script.js
+++ Course Projects/JavaScript Algorithms and Data Structures (Beta)/02. Learn Form Validation by Building a Calorie Counter/script.js
@@ -1,24 +1,27 @@
-let isError = false;
-
 const calorieCounter = document.getElementById('calorie-counter');
 const budgetNumberInput = document.getElementById('budget');
 const entryDropdown = document.getElementById('entry-dropdown');
 const addEntryButton = document.getElementById('add-entry');
 const clearButton = document.getElementById('clear');
 const output = document.getElementById('output');
+let isError = false;
 
-/* ONE WAY TO CLEAN STRING!!!
-function cleanInputString(str) {
-    const strArray = str.split('');
-    let cleanStrArray = [];
-    for (let i = 0; i < strArray.length; i++) {
-        if (!["+", "-", " "].includes(strArray[i])) {
-            cleanStrArray.push(strArray[i]);
-        }        
-    }
-}
-*/
 function cleanInputString(str) {
     const regex = /[+-\s]/g;
     return str.replace(regex, "");
 }  
+
+function isInvalidInput(str) {
+    const regex = /\d+e\d+/i;
+    return str.match(regex);
+}
+
+function addEntry() {
+    const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`);
+    const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length;
+    let HTMLString = `
+    <label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
+    <input type="text" placeholder="Name" id="${entryDropdown.value}-${entryNumber}-name">
+    <label for="${entryDropdown.value}-${entryNumber}-calories">Entry ${entryNumber} Calories</label>
+    <input type="number" min="0" placeholder="Calories" id="${entryDropdown.value}-${entryNumber}-calories">`;
+}
